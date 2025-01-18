da489a455dbd4452ebe0f45e60aa03566df96434
Emily <exie28@hb.edu>
2025-01-18 15:15:42 -0500

    renamed and removed the turning from the objects

--- src/main/java/frc/robot/subsystems/algaeEjector/AlgaeEjectorHardware.java
+++ src/main/java/frc/robot/subsystems/algaeEjector/AlgaeEjectorHardware.java
@@ -8,32 +8,32 @@ import frc.robot.Constants.MotorConstants;
 
 public class AlgaeEjectorHardware implements AlgaeEjectorIO {
 
-    @Override
-    public void setSpeed(double speed) {
+        private static final SparkMaxConfig SPARK_MAX_CONFIG = new SparkMaxConfig();
+        private static final boolean ENCODER_INVERTED = true;
+        private static final SparkBaseConfig.IdleMode IDLE_MODE = SparkBaseConfig.IdleMode.kBrake;
+        private static final double ENCODER_POSITION_FACTOR = (2 * Math.PI); // radians
+        private static final double ENCODER_VELOCITY_FACTOR = (2 * Math.PI) / 60.0; // radians per second
 
-    }
+        private static final double P = 1.0;
+        private static final double I = 0;
+        private static final double D = 0.001;
+        private static final double FF = 0;
+        private static final double MIN_OUTPUT = -1;
+        private static final double MAX_OUTPUT = 1;
 
-    private static final SparkMaxConfig SPARK_MAX_CONFIG_TURNING = new SparkMaxConfig();
-    private static final boolean TURNING_ENCODER_INVERTED = true;
-    private static final SparkBaseConfig.IdleMode TURNING_MOTOR_IDLE_MODE = SparkBaseConfig.IdleMode.kBrake;
-    private static final double TURNING_ENCODER_POSITION_FACTOR = (2 * Math.PI); // radians
-    private static final double TURNING_ENCODER_VELOCITY_FACTOR = (2 * Math.PI) / 60.0; // radians per second
+        public AlgaeEjectorHardware() {
 
-    private static final double TURNING_P = 1.0;
-    private static final double TURNING_I = 0;
-    private static final double TURNING_D = 0.001;
-    private static final double TURNING_FF = 0;
-    private static final double TURNING_MIN_OUTPUT = -1;
-    private static final double TURNING_MAX_OUTPUT = 1;
+                SPARK_MAX_CONFIG.inverted(ENCODER_INVERTED).idleMode(IDLE_MODE)
+                                .smartCurrentLimit(MotorConstants.NEO550_CURRENT_LIMIT);
+                SPARK_MAX_CONFIG.encoder.positionConversionFactor(ENCODER_POSITION_FACTOR)
+                                .velocityConversionFactor(ENCODER_VELOCITY_FACTOR);
+                SPARK_MAX_CONFIG.closedLoop.feedbackSensor(FeedbackSensor.kPrimaryEncoder)
+                                .pidf(P, I, D, FF)
+                                .outputRange(MIN_OUTPUT, MAX_OUTPUT);
+        }
 
-    public AlgaeEjectorHardware() {
-
-        SPARK_MAX_CONFIG_TURNING.inverted(TURNING_ENCODER_INVERTED).idleMode(TURNING_MOTOR_IDLE_MODE)
-                .smartCurrentLimit(MotorConstants.NEO550_CURRENT_LIMIT);
-        SPARK_MAX_CONFIG_TURNING.encoder.positionConversionFactor(TURNING_ENCODER_POSITION_FACTOR)
-                .velocityConversionFactor(TURNING_ENCODER_VELOCITY_FACTOR);
-        SPARK_MAX_CONFIG_TURNING.closedLoop.feedbackSensor(FeedbackSensor.kPrimaryEncoder)
-                .pidf(TURNING_P, TURNING_I, TURNING_D, TURNING_FF).outputRange(TURNING_MIN_OUTPUT, TURNING_MAX_OUTPUT);
-    }
+        @Override
+        public void setSpeed(double speed) {
+        }
 
 }
