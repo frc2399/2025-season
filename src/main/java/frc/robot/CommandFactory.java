64db248fe0a654946cdf72c3c4b7f04afdce16f4
Aashvi Jagetia <ajagetia26@hb.edu>
2025-02-27 18:56:21 -0500

    replaced outtake with new method in command factory

--- src/main/java/frc/robot/CommandFactory.java
+++ src/main/java/frc/robot/CommandFactory.java
@@ -117,7 +117,8 @@ public class CommandFactory {
   public Command outtakeBasedOnMode(Supplier<GameMode> gameMode) {
     return Commands.either(
         algaeIntake.outtake(),
-        coralIntake.outtake(),
+        // coralIntake.outtake(),
+        coralIntake.setOuttakeSpeed(() -> getSetpoint()),
         () -> (getGameMode() == GameMode.ALGAE));
   }
 
