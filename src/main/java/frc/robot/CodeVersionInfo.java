ca98b4c35fc221132406aab1308664825c5fdd1f
Emily <exie28@hb.edu>
2025-03-11 20:53:26 -0400

    removing something that is never used

--- src/main/java/frc/robot/CodeVersionInfo.java
+++ src/main/java/frc/robot/CodeVersionInfo.java
@@ -7,12 +7,12 @@ public final class CodeVersionInfo {
 	public static final String MAVEN_GROUP = "";
 	public static final String MAVEN_NAME = "2025-season";
 	public static final String VERSION = "unspecified";
-	public static final int GIT_REVISION = 623;
-	public static final String GIT_SHA = "db3b012fc044545baf7b35e8152cb14458e69125";
-	public static final String GIT_DATE = "2025-03-11 19:48";
+	public static final int GIT_REVISION = 624;
+	public static final String GIT_SHA = "65b740f4001c9fe254a701a8bc890ee992afa5bb";
+	public static final String GIT_DATE = "2025-03-11 19:49";
 	public static final String GIT_BRANCH = "code-clean-up";
-	public static final String BUILD_DATE = "2025-03-11 19:48";
-	public static final long BUILD_UNIX_TIME = 1741740533823L;
+	public static final String BUILD_DATE = "2025-03-11 19:53";
+	public static final long BUILD_UNIX_TIME = 1741740793163L;
 	public static final int DIRTY = 1;
 
 	private CodeVersionInfo(){}
