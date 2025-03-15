12cb7d60cae3d0682d145c3ee7800596fb5d1c3d
mhoughton26 <mhoughton26@hb.edu>
2025-03-15 14:23:11 -0400

    working L1 autons!

--- src/main/java/frc/robot/CodeVersionInfo.java
+++ src/main/java/frc/robot/CodeVersionInfo.java
@@ -7,13 +7,13 @@ public final class CodeVersionInfo {
 	public static final String MAVEN_GROUP = "";
 	public static final String MAVEN_NAME = "2025-season";
 	public static final String VERSION = "unspecified";
-	public static final int GIT_REVISION = 640;
-	public static final String GIT_SHA = "ce78d50aa6d2a775409ac701b1b1f23395758dca";
-	public static final String GIT_DATE = "2025-03-15 11:51";
-	public static final String GIT_BRANCH = "fixing-gyro-fresh-two";
-	public static final String BUILD_DATE = "2025-03-15 12:04";
-	public static final long BUILD_UNIX_TIME = 1742058275789L;
-	public static final int DIRTY = 0;
+	public static final int GIT_REVISION = 644;
+	public static final String GIT_SHA = "7ea7622ab6c101ae37f8dae8acde06617e96f6a3";
+	public static final String GIT_DATE = "2025-03-15 12:48";
+	public static final String GIT_BRANCH = "updating-swerve-module-locations";
+	public static final String BUILD_DATE = "2025-03-15 13:18";
+	public static final long BUILD_UNIX_TIME = 1742062736855L;
+	public static final int DIRTY = 1;
 
 	private CodeVersionInfo(){}
 }
