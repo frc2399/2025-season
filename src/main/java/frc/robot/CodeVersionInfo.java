dc6866263beeb36c6e49d49ac5333288b5eb3bbc
mhoughton26 <mhoughton26@hb.edu>
2025-03-15 14:56:57 -0400

    removing weird markers

--- src/main/java/frc/robot/CodeVersionInfo.java
+++ src/main/java/frc/robot/CodeVersionInfo.java
@@ -7,12 +7,12 @@ public final class CodeVersionInfo {
 	public static final String MAVEN_GROUP = "";
 	public static final String MAVEN_NAME = "2025-season";
 	public static final String VERSION = "unspecified";
-	public static final int GIT_REVISION = 644;
-	public static final String GIT_SHA = "7ea7622ab6c101ae37f8dae8acde06617e96f6a3";
-	public static final String GIT_DATE = "2025-03-15 12:48";
-	public static final String GIT_BRANCH = "updating-swerve-module-locations";
-	public static final String BUILD_DATE = "2025-03-15 13:18";
-	public static final long BUILD_UNIX_TIME = 1742062736855L;
+	public static final int GIT_REVISION = 650;
+	public static final String GIT_SHA = "a049872618b8dac7412facb51fb23e0a54fa9116";
+	public static final String GIT_DATE = "2025-03-15 13:28";
+	public static final String GIT_BRANCH = "l4-auton";
+	public static final String BUILD_DATE = "2025-03-15 13:54";
+	public static final long BUILD_UNIX_TIME = 1742064853411L;
 	public static final int DIRTY = 1;
 
 	private CodeVersionInfo(){}
