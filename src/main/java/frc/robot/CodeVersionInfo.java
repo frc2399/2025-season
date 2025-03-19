e740fffa215473cb08d9fd44c8460be63508dc25
mhoughton26 <mhoughton26@hb.edu>
2025-03-18 21:00:04 -0400

    L4 AUTON RIGHT SIDE!

--- src/main/java/frc/robot/CodeVersionInfo.java
+++ src/main/java/frc/robot/CodeVersionInfo.java
@@ -7,12 +7,12 @@ public final class CodeVersionInfo {
 	public static final String MAVEN_GROUP = "";
 	public static final String MAVEN_NAME = "2025-season";
 	public static final String VERSION = "unspecified";
-	public static final int GIT_REVISION = 653;
-	public static final String GIT_SHA = "787285d3bef940b2bb123ed43e87cb09d022ecbe";
-	public static final String GIT_DATE = "2025-03-15 15:14";
-	public static final String GIT_BRANCH = "wpilib-updates";
-	public static final String BUILD_DATE = "2025-03-18 17:49";
-	public static final long BUILD_UNIX_TIME = 1742338183991L;
+	public static final int GIT_REVISION = 658;
+	public static final String GIT_SHA = "b262f7bba1af34083613f1940cb0ca155adcf10e";
+	public static final String GIT_DATE = "2025-03-18 19:44";
+	public static final String GIT_BRANCH = "l4-auton-right-side";
+	public static final String BUILD_DATE = "2025-03-18 19:54";
+	public static final long BUILD_UNIX_TIME = 1742345653822L;
 	public static final int DIRTY = 1;
 
 	private CodeVersionInfo(){}
