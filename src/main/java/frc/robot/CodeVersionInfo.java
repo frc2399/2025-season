17f91da2b55829d3a0365438b442560c301ac154
mhoughton26 <mhoughton26@hb.edu>
2025-03-20 16:30:14 -0400

    more drafting multipiece l4

--- src/main/java/frc/robot/CodeVersionInfo.java
+++ src/main/java/frc/robot/CodeVersionInfo.java
@@ -7,12 +7,12 @@ public final class CodeVersionInfo {
 	public static final String MAVEN_GROUP = "";
 	public static final String MAVEN_NAME = "2025-season";
 	public static final String VERSION = "unspecified";
-	public static final int GIT_REVISION = 748;
-	public static final String GIT_SHA = "afcdc1f67ae5b9d98f680bfac66c723378c8c725";
-	public static final String GIT_DATE = "2025-03-20 11:33";
+	public static final int GIT_REVISION = 754;
+	public static final String GIT_SHA = "672f975f2116f7f8e8ebec5dc18bb7156b170441";
+	public static final String GIT_DATE = "2025-03-20 13:55";
 	public static final String GIT_BRANCH = "pittsburgh";
-	public static final String BUILD_DATE = "2025-03-20 12:14";
-	public static final long BUILD_UNIX_TIME = 1742490854338L;
+	public static final String BUILD_DATE = "2025-03-20 13:57";
+	public static final long BUILD_UNIX_TIME = 1742497048329L;
 	public static final int DIRTY = 1;
 
 	private CodeVersionInfo(){}
