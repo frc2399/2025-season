afcdc1f67ae5b9d98f680bfac66c723378c8c725
mhoughton26 <mhoughton26@hb.edu>
2025-03-20 12:33:57 -0400

    use proper currentAngle variable when on red

--- src/main/java/frc/robot/CodeVersionInfo.java
+++ src/main/java/frc/robot/CodeVersionInfo.java
@@ -7,12 +7,12 @@ public final class CodeVersionInfo {
 	public static final String MAVEN_GROUP = "";
 	public static final String MAVEN_NAME = "2025-season";
 	public static final String VERSION = "unspecified";
-	public static final int GIT_REVISION = 746;
-	public static final String GIT_SHA = "21bd1f80ede54564c1539aa1c2c8744bdb509ee6";
-	public static final String GIT_DATE = "2025-03-20 06:39";
+	public static final int GIT_REVISION = 747;
+	public static final String GIT_SHA = "26ca6d2d3b9ca65e671dcc71ff81ba8b8bc21123";
+	public static final String GIT_DATE = "2025-03-20 09:58";
 	public static final String GIT_BRANCH = "pittsburgh";
-	public static final String BUILD_DATE = "2025-03-20 08:54";
-	public static final long BUILD_UNIX_TIME = 1742478889837L;
+	public static final String BUILD_DATE = "2025-03-20 10:52";
+	public static final long BUILD_UNIX_TIME = 1742485943417L;
 	public static final int DIRTY = 1;
 
 	private CodeVersionInfo(){}
