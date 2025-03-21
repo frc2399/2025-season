ef257ec1aa3efc380d84161cc16fd5a2acba2af4
mhoughton26 <mhoughton26@hb.edu>
2025-03-21 11:30:11 -0400

    l4 auton SPEEDIER!!!

--- src/main/java/frc/robot/CodeVersionInfo.java
+++ src/main/java/frc/robot/CodeVersionInfo.java
@@ -7,13 +7,13 @@ public final class CodeVersionInfo {
 	public static final String MAVEN_GROUP = "";
 	public static final String MAVEN_NAME = "2025-season";
 	public static final String VERSION = "unspecified";
-	public static final int GIT_REVISION = 761;
-	public static final String GIT_SHA = "cd7dc918cf3242d90fbc5365a8164496f29413d8";
-	public static final String GIT_DATE = "2025-03-20 17:07";
+	public static final int GIT_REVISION = 765;
+	public static final String GIT_SHA = "7c4a4f9251ce789bc2b79c1b79277f379f0a9ddc";
+	public static final String GIT_DATE = "2025-03-21 08:41";
 	public static final String GIT_BRANCH = "pittsburgh";
-	public static final String BUILD_DATE = "2025-03-20 17:16";
-	public static final long BUILD_UNIX_TIME = 1742508991267L;
-	public static final int DIRTY = 1;
+	public static final String BUILD_DATE = "2025-03-21 08:45";
+	public static final long BUILD_UNIX_TIME = 1742564743156L;
+	public static final int DIRTY = 0;
 
 	private CodeVersionInfo(){}
 }
