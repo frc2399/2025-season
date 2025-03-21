00d2b5b9108a8f1e88278df60ad246aa30e4e3b0
mhoughton26 <mhoughton26@hb.edu>
2025-03-21 15:58:28 -0400

    swap to blue offsets

--- src/main/java/frc/robot/CodeVersionInfo.java
+++ src/main/java/frc/robot/CodeVersionInfo.java
@@ -7,13 +7,13 @@ public final class CodeVersionInfo {
 	public static final String MAVEN_GROUP = "";
 	public static final String MAVEN_NAME = "2025-season";
 	public static final String VERSION = "unspecified";
-	public static final int GIT_REVISION = 766;
-	public static final String GIT_SHA = "ef257ec1aa3efc380d84161cc16fd5a2acba2af4";
-	public static final String GIT_DATE = "2025-03-21 10:30";
+	public static final int GIT_REVISION = 769;
+	public static final String GIT_SHA = "d159ef231d165e3071ba6457f65289ae11ea8cda";
+	public static final String GIT_DATE = "2025-03-21 14:35";
 	public static final String GIT_BRANCH = "pittsburgh";
-	public static final String BUILD_DATE = "2025-03-21 12:00";
-	public static final long BUILD_UNIX_TIME = 1742576418289L;
-	public static final int DIRTY = 1;
+	public static final String BUILD_DATE = "2025-03-21 14:36";
+	public static final long BUILD_UNIX_TIME = 1742585763850L;
+	public static final int DIRTY = 0;
 
 	private CodeVersionInfo(){}
 }
