1473b9df2e6389f0f4a99d2483d7d7344f42f636
mhoughton26 <mhoughton26@hb.edu>
2025-03-22 12:19:16 -0400

    fix passive intake

--- src/main/java/frc/robot/CodeVersionInfo.java
+++ src/main/java/frc/robot/CodeVersionInfo.java
@@ -7,13 +7,13 @@ public final class CodeVersionInfo {
 	public static final String MAVEN_GROUP = "";
 	public static final String MAVEN_NAME = "2025-season";
 	public static final String VERSION = "unspecified";
-	public static final int GIT_REVISION = 770;
-	public static final String GIT_SHA = "00d2b5b9108a8f1e88278df60ad246aa30e4e3b0";
-	public static final String GIT_DATE = "2025-03-21 14:58";
+	public static final int GIT_REVISION = 778;
+	public static final String GIT_SHA = "ea9d5023dc5f7e8042f98960090e7b67c67af1f8";
+	public static final String GIT_DATE = "2025-03-22 08:54";
 	public static final String GIT_BRANCH = "pittsburgh";
-	public static final String BUILD_DATE = "2025-03-21 14:58";
-	public static final long BUILD_UNIX_TIME = 1742587139824L;
-	public static final int DIRTY = 0;
+	public static final String BUILD_DATE = "2025-03-22 11:17";
+	public static final long BUILD_UNIX_TIME = 1742660240042L;
+	public static final int DIRTY = 1;
 
 	private CodeVersionInfo(){}
 }
