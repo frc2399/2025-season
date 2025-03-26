1aa7fc759a61f8b0b61513adf303eb2cdfb6e632
Clare Hess <chess25@hb.edu>
2025-03-25 20:47:19 -0400

    increased driving P

--- src/main/java/frc/robot/CodeVersionInfo.java
+++ src/main/java/frc/robot/CodeVersionInfo.java
@@ -7,12 +7,12 @@ public final class CodeVersionInfo {
 	public static final String MAVEN_GROUP = "";
 	public static final String MAVEN_NAME = "2025-season";
 	public static final String VERSION = "unspecified";
-	public static final int GIT_REVISION = 782;
-	public static final String GIT_SHA = "35f7f85b2fdb617a39a2acb6b667be3537fb16d7";
-	public static final String GIT_DATE = "2025-03-25 17:58";
+	public static final int GIT_REVISION = 783;
+	public static final String GIT_SHA = "c8a950f15518d82bc49bdf091f6030d0cab1f98b";
+	public static final String GIT_DATE = "2025-03-25 19:16";
 	public static final String GIT_BRANCH = "tune-driving";
-	public static final String BUILD_DATE = "2025-03-25 19:07";
-	public static final long BUILD_UNIX_TIME = 1742947642812L;
+	public static final String BUILD_DATE = "2025-03-25 19:43";
+	public static final long BUILD_UNIX_TIME = 1742949806106L;
 	public static final int DIRTY = 1;
 
 	private CodeVersionInfo(){}
--- src/main/java/frc/robot/subsystems/drive/SwerveModuleHardwareVortex.java
+++ src/main/java/frc/robot/subsystems/drive/SwerveModuleHardwareVortex.java
@@ -82,10 +82,10 @@ public class SwerveModuleHardwareVortex implements SwerveModuleIO {
     private static final double TURNING_ENCODER_POSITION_PID_MIN_INPUT = 0; // radians
     private static final double TURNING_ENCODER_POSITION_PID_MAX_INPUT = TURNING_ENCODER_POSITION_FACTOR; // radians
 
-    private static final double DRIVING_P = 0.3;
+    private static final double DRIVING_P = 0.4;
     private static final double DRIVING_I = 0;
     private static final double DRIVING_D = 0;
-    private static final double DRIVING_FF = 1 / (DRIVE_WHEEL_FREE_SPEED.in(RotationsPerSecond) * WHEEL_CIRCUMFERENCE.in(Meters));
+    private static final double DRIVING_FF = 1 / (DRIVE_WHEEL_FREE_SPEED.in(RotationsPerSecond));
     private static final double DRIVING_MIN_OUTPUT = -1;
     private static final double DRIVING_MAX_OUTPUT = 1;
 
