d447e276fbdd155ba83041b1304ccd823ad1ef27
mhoughton26 <mhoughton26@hb.edu>
2025-03-29 15:29:36 -0400

    change camera constants

--- src/main/java/frc/robot/CodeVersionInfo.java
+++ src/main/java/frc/robot/CodeVersionInfo.java
@@ -7,12 +7,12 @@ public final class CodeVersionInfo {
 	public static final String MAVEN_GROUP = "";
 	public static final String MAVEN_NAME = "2025-season";
 	public static final String VERSION = "unspecified";
-	public static final int GIT_REVISION = 784;
-	public static final String GIT_SHA = "1aa7fc759a61f8b0b61513adf303eb2cdfb6e632";
-	public static final String GIT_DATE = "2025-03-25 19:47";
-	public static final String GIT_BRANCH = "tune-driving";
-	public static final String BUILD_DATE = "2025-03-27 18:25";
-	public static final long BUILD_UNIX_TIME = 1743117958554L;
+	public static final int GIT_REVISION = 787;
+	public static final String GIT_SHA = "ecb1dc34289760b3d37dfa4e81db5264a08c9795";
+	public static final String GIT_DATE = "2025-03-27 18:30";
+	public static final String GIT_BRANCH = "camera-mount-swap";
+	public static final String BUILD_DATE = "2025-03-29 14:06";
+	public static final long BUILD_UNIX_TIME = 1743275163278L;
 	public static final int DIRTY = 1;
 
 	private CodeVersionInfo(){}
