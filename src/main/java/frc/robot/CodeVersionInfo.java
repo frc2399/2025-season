8f5e4f2471e93e996a586b0a0973b08afa97a050
mhoughton26 <mhoughton26@hb.edu>
2025-03-29 12:55:09 -0400

    sending the auton to will for server farming

--- src/main/java/frc/robot/CodeVersionInfo.java
+++ src/main/java/frc/robot/CodeVersionInfo.java
@@ -7,12 +7,12 @@ public final class CodeVersionInfo {
 	public static final String MAVEN_GROUP = "";
 	public static final String MAVEN_NAME = "2025-season";
 	public static final String VERSION = "unspecified";
-	public static final int GIT_REVISION = 787;
-	public static final String GIT_SHA = "ecb1dc34289760b3d37dfa4e81db5264a08c9795";
-	public static final String GIT_DATE = "2025-03-27 18:30";
+	public static final int GIT_REVISION = 788;
+	public static final String GIT_SHA = "24dc37b54752ac08d8b42cadb94f8b5dc349689a";
+	public static final String GIT_DATE = "2025-03-27 19:55";
 	public static final String GIT_BRANCH = "auton-tuning";
-	public static final String BUILD_DATE = "2025-03-27 19:37";
-	public static final long BUILD_UNIX_TIME = 1743122264102L;
+	public static final String BUILD_DATE = "2025-03-29 11:33";
+	public static final long BUILD_UNIX_TIME = 1743266018752L;
 	public static final int DIRTY = 1;
 
 	private CodeVersionInfo(){}
