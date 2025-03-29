6099f98c72e7cca64bff7424f182f811c7144bcf
mhoughton26 <mhoughton26@hb.edu>
2025-03-29 17:04:10 -0400

    more noodling - test this when possible - change prescore pose

--- src/main/java/frc/robot/CodeVersionInfo.java
+++ src/main/java/frc/robot/CodeVersionInfo.java
@@ -7,13 +7,13 @@ public final class CodeVersionInfo {
 	public static final String MAVEN_GROUP = "";
 	public static final String MAVEN_NAME = "2025-season";
 	public static final String VERSION = "unspecified";
-	public static final int GIT_REVISION = 793;
-	public static final String GIT_SHA = "25809a6289bb9a8aa4f8fd4b438e07c3bb564115";
-	public static final String GIT_DATE = "2025-03-29 14:32";
+	public static final int GIT_REVISION = 794;
+	public static final String GIT_SHA = "aecbc9ba7f6958207fcfc5b34e473fce010e5197";
+	public static final String GIT_DATE = "2025-03-29 15:46";
 	public static final String GIT_BRANCH = "auton-tuning";
-	public static final String BUILD_DATE = "2025-03-29 15:28";
-	public static final long BUILD_UNIX_TIME = 1743280119921L;
-	public static final int DIRTY = 1;
+	public static final String BUILD_DATE = "2025-03-29 15:59";
+	public static final long BUILD_UNIX_TIME = 1743281968660L;
+	public static final int DIRTY = 0;
 
 	private CodeVersionInfo(){}
 }
