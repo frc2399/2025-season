d0ffaff5152a1f6d51a6c983ecbbfe536a7a2379
Clare Hess <chess25@hb.edu>
2025-03-30 15:49:03 -0400

    changed coral wrist intake angle

--- src/main/java/frc/robot/CodeVersionInfo.java
+++ src/main/java/frc/robot/CodeVersionInfo.java
@@ -7,12 +7,12 @@ public final class CodeVersionInfo {
 	public static final String MAVEN_GROUP = "";
 	public static final String MAVEN_NAME = "2025-season";
 	public static final String VERSION = "unspecified";
-	public static final int GIT_REVISION = 787;
-	public static final String GIT_SHA = "ecb1dc34289760b3d37dfa4e81db5264a08c9795";
-	public static final String GIT_DATE = "2025-03-27 18:30";
-	public static final String GIT_BRANCH = "camera-mount-swap";
-	public static final String BUILD_DATE = "2025-03-29 14:06";
-	public static final long BUILD_UNIX_TIME = 1743275163278L;
+	public static final int GIT_REVISION = 789;
+	public static final String GIT_SHA = "6d6b37ef7e085212a201b5cfeccc6ba111e1cb0c";
+	public static final String GIT_DATE = "2025-03-29 14:32";
+	public static final String GIT_BRANCH = "pre-buckeye-drive-practice-changes";
+	public static final String BUILD_DATE = "2025-03-30 11:10";
+	public static final long BUILD_UNIX_TIME = 1743351052209L;
 	public static final int DIRTY = 1;
 
 	private CodeVersionInfo(){}
