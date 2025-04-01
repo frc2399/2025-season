841c6fc6839feb739b773a6744f80e1ca9038ddb
Maheeshah07 <mshah25@hb.edu>
2025-03-31 20:08:23 -0400

    auton changes

--- src/main/java/frc/robot/CodeVersionInfo.java
+++ src/main/java/frc/robot/CodeVersionInfo.java
@@ -5,14 +5,15 @@ package frc.robot;
  */
 public final class CodeVersionInfo {
 	public static final String MAVEN_GROUP = "";
-	public static final String MAVEN_NAME = "2025-season";
+	public static final String MAVEN_NAME = "2025-season-1";
 	public static final String VERSION = "unspecified";
-	public static final int GIT_REVISION = 797;
-	public static final String GIT_SHA = "4433a07426c3fc3462918a03e021c5a2789ca6fc";
-	public static final String GIT_DATE = "2025-03-29 19:15";
+	public static final int GIT_REVISION = 802;
+	public static final String GIT_SHA = "3db2a2d75198c93d7ec943ebd14435c7d6ab5561";
+	public static final String GIT_DATE = "2025-03-31 18:26";
 	public static final String GIT_BRANCH = "auton-tuning";
-	public static final String BUILD_DATE = "2025-03-30 15:42";
-	public static final long BUILD_UNIX_TIME = 1743367377815L;
-	public static final int DIRTY = 1;
+	public static final String BUILD_DATE = "2025-03-31 19:02";
+	public static final long BUILD_UNIX_TIME = 1743465764188L;
+	public static final int DIRTY = 0;
+
 	private CodeVersionInfo(){}
 }
