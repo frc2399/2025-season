8c5d9be82d42ddd163090c27778fde45e48c26dc
mhoughton26 <mhoughton26@hb.edu>
2025-04-05 09:55:19 -0400

    remove reset odometries out of autons

--- src/main/java/frc/robot/CodeVersionInfo.java
+++ src/main/java/frc/robot/CodeVersionInfo.java
@@ -7,12 +7,12 @@ public final class CodeVersionInfo {
 	public static final String MAVEN_GROUP = "";
 	public static final String MAVEN_NAME = "2025-season";
 	public static final String VERSION = "unspecified";
-	public static final int GIT_REVISION = 824;
-	public static final String GIT_SHA = "268ce671100df019e4d7dfb930998824cfb8b97b";
-	public static final String GIT_DATE = "2025-04-03 21:08";
+	public static final int GIT_REVISION = 825;
+	public static final String GIT_SHA = "64fc6d1e1f23640c103e9e02b0933b719a7ad268";
+	public static final String GIT_DATE = "2025-04-04 10:20";
 	public static final String GIT_BRANCH = "buckeye";
-	public static final String BUILD_DATE = "2025-04-04 09:34";
-	public static final long BUILD_UNIX_TIME = 1743777292616L;
+	public static final String BUILD_DATE = "2025-04-04 13:58";
+	public static final long BUILD_UNIX_TIME = 1743793082173L;
 	public static final int DIRTY = 1;
 
 	private CodeVersionInfo(){}
