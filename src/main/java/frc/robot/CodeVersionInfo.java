78eed743858d4ee67d2ef7f007c870757a28bbaf
mhoughton26 <mhoughton26@hb.edu>
2025-08-28 18:31:34 -0400

    removing default behavior for sysid-ing

--- src/main/java/frc/robot/CodeVersionInfo.java
+++ src/main/java/frc/robot/CodeVersionInfo.java
@@ -7,12 +7,12 @@ public final class CodeVersionInfo {
 	public static final String MAVEN_GROUP = "";
 	public static final String MAVEN_NAME = "2025-season";
 	public static final String VERSION = "unspecified";
-	public static final int GIT_REVISION = 826;
-	public static final String GIT_SHA = "8c5d9be82d42ddd163090c27778fde45e48c26dc";
-	public static final String GIT_DATE = "2025-04-05 08:55";
-	public static final String GIT_BRANCH = "buckeye";
-	public static final String BUILD_DATE = "2025-04-05 14:01";
-	public static final long BUILD_UNIX_TIME = 1743879661353L;
+	public static final int GIT_REVISION = 830;
+	public static final String GIT_SHA = "93c7b1faa0ed90aea000b8f0055d2afb205985ae";
+	public static final String GIT_DATE = "2025-08-21 18:44";
+	public static final String GIT_BRANCH = "sysid";
+	public static final String BUILD_DATE = "2025-08-28 17:30";
+	public static final long BUILD_UNIX_TIME = 1756420206966L;
 	public static final int DIRTY = 1;
 
 	private CodeVersionInfo(){}
